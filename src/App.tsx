ef639fc59783a8cb7320ddf6bc827df5324d8f44
Foysal Mohammad <foysal@earlblue.com>
2021-06-15 17:05:51 +0100

    useState with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from 'react'
 function App() {
+  const [people, setPeople] = useState<{ id: number; value: number }[]>([])
+
+  const addpeople = () => {
+    setPeople([
+      ...people,
+      { id: people.length, value: Math.floor(Math.random() * 10) },
+    ])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>People Invited to my Party</h1>
+      <button onClick={addpeople}>AddPeople</button>
+      <ul>
+        {people.map((el) => (
+          <li key={el.id}>{el.value}</li>
+        ))}
+      </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
